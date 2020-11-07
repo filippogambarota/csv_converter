bcb959d385790e9c46b6efcad4f6b7b8d492d999
Filippo Gambarota <filippo.gambarota@gmail.com>
2020-11-07 12:34:16 +0100

    init repo

---
+++ csv_converter.R
@@ -0,0 +1,17 @@
+# CSV Converter
+
+# Packages
+
+require(WriteXLS)
+
+# Import Data
+
+all_files <- list.files("csv", pattern = ".csv") # read all csv names
+
+all_csv <- lapply(all_files, function(x) read.csv(file = paste0("csv/", x), header = T)) # read all csv files
+
+# Write Excel files
+
+all_files <- gsub(".csv", ".xlsx", all_files)
+
+mapply(function(x, y) WriteXLS(x, paste0("excel/", y)), all_csv, all_files)
